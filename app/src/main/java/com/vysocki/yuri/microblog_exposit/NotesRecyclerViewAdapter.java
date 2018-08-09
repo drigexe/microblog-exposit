1a4e5a0ddd37c510d6a1cdc05afc0056bd058b5c
Yuri Vysocki <drigexe@gmail.com>
2018-08-09 16:34:25 +0300

    recreated recyclerview with adapter from scratch

---
+++ app/src/main/java/com/vysocki/yuri/microblog_exposit/NotesRecyclerViewAdapter.java
@@ -0,0 +1,72 @@
+package com.vysocki.yuri.microblog_exposit;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class NotesRecyclerViewAdapter extends RecyclerView.Adapter<NotesRecyclerViewAdapter.ViewHolder> {
+
+    private static final String TAG = "NoteRecyclerViewAdapter";
+
+    private ArrayList<String> mNoteThemes = new ArrayList<>();
+    private ArrayList<String> mNoteDates = new ArrayList<>();
+    private Context mContext;
+
+    public NotesRecyclerViewAdapter(Context mContext, ArrayList<String> mNoteThemes, ArrayList<String> mNoteDates) {
+        this.mNoteThemes = mNoteThemes;
+        this.mNoteDates = mNoteDates;
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_item, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+        Log.d(TAG, "onBindViewHolder: called");
+
+        holder.themeTextView.setText(mNoteThemes.get(position));
+        holder.dateTextView.setText(mNoteDates.get(position));
+
+        holder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: clicled on" + mNoteThemes.get(position));
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mNoteThemes.size();
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView themeTextView;
+        TextView dateTextView;
+        RelativeLayout parentLayout;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            themeTextView = itemView.findViewById(R.id.noteThemeText);
+            dateTextView = itemView.findViewById(R.id.noteDateText);
+            parentLayout = itemView.findViewById(R.id.recycler_parent_layout);
+        }
+    }
+}
--- app/src/main/java/com/vysocki/yuri/microblog_exposit/fragments/internal/NotesListFragment.java
+++ app/src/main/java/com/vysocki/yuri/microblog_exposit/fragments/internal/NotesListFragment.java
@@ -7,15 +7,14 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
 
 import com.vysocki.yuri.microblog_exposit.MainActivity;
-import com.vysocki.yuri.microblog_exposit.MyAdapter;
 import com.vysocki.yuri.microblog_exposit.Note;
+import com.vysocki.yuri.microblog_exposit.NotesRecyclerViewAdapter;
 import com.vysocki.yuri.microblog_exposit.R;
 import com.vysocki.yuri.microblog_exposit.SharedViewModel;
 
@@ -23,13 +22,14 @@ import java.util.ArrayList;
 
 public class NotesListFragment extends Fragment {
 
-    private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
+    private static final String TAG = "NotesListFragment";
+
+    private ArrayList<String> mNoteThemes = new ArrayList<>();
+    private ArrayList<String> mNoteDates = new ArrayList<>();
 
     private SharedViewModel viewModel;
 
-    Note note = new Note();
+    private Note note = new Note();
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -43,26 +43,8 @@ public class NotesListFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_notes_list, container, false);
 
-        String[] myDataset = new String[10];
-        myDataset[0] = "kekus";
-        myDataset[1] = "LOLOL";
-        myDataset[2] = "AZAZAZ";
-        myDataset[3] = "AZAZ4325AZ";
-        myDataset[4] = "AZA1ZAZ";
-        myDataset[5] = "AZA432ZAZ";
-        myDataset[6] = "AZA2ZAZ";
-        myDataset[7] = "LOLrewtOL";
-        myDataset[8] = "LOLweweOL";
-        myDataset[9] = "LOL666OL";
-
-        mRecyclerView = view.findViewById(R.id.my_recycler_view);
-        mRecyclerView.setHasFixedSize(true);
-        mLayoutManager = new LinearLayoutManager(getActivity());
-        mRecyclerView.setLayoutManager(mLayoutManager);
-        mAdapter = new MyAdapter(myDataset);
-        mRecyclerView.setAdapter(mAdapter);
-
-
+        initNoteArrays();
+        initRecyclerView(view);
 
         /*button1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -78,6 +60,49 @@ public class NotesListFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         ((MainActivity)getActivity()).toggleDrawer(false);
+    }
+
+    public void initNoteArrays() {
+        //get mote items from viewmodel and add them to the mNoteThemes and mNoteDates
+        mNoteThemes.add("kokoko");
+        mNoteDates.add("kekasaeke");
+        mNoteThemes.add("kokdsaaoko");
+        mNoteDates.add("kekhgjheke");
+        mNoteThemes.add("kodfgfdkoko");
+        mNoteDates.add("kek54545eke");
+        mNoteThemes.add("kokoko");
+        mNoteDates.add("kekasaeke");
+        mNoteThemes.add("kokdsaaoko");
+        mNoteDates.add("kekhgjheke");
+        mNoteThemes.add("kodfgfdkoko");
+        mNoteDates.add("kek54545eke");
+        mNoteThemes.add("kokoko");
+        mNoteDates.add("kekasaeke");
+        mNoteThemes.add("kokdsaaoko");
+        mNoteDates.add("kekhgjheke");
+        mNoteThemes.add("kodfgfdkoko");
+        mNoteDates.add("kek54545eke");
+        mNoteThemes.add("kokoko");
+        mNoteDates.add("kekasaeke");
+        mNoteThemes.add("kokdsaaoko");
+        mNoteDates.add("kekhgjheke");
+        mNoteThemes.add("kodfgfdkoko");
+        mNoteDates.add("kek54545eke");
+        mNoteThemes.add("kokoko");
+        mNoteDates.add("kekasaeke");
+        mNoteThemes.add("kokdsaaoko");
+        mNoteDates.add("kekhgjheke");
+        mNoteThemes.add("kodfgfdkoko");
+        mNoteDates.add("kek54545eke");
+    }
+
+    public void initRecyclerView(View view) {
+        Log.d(TAG, "initRecyclerView: init recyclerview.");
+
+        RecyclerView recyclerView = view.findViewById(R.id.my_recycler_view);
+        NotesRecyclerViewAdapter adapter = new NotesRecyclerViewAdapter(getActivity(), mNoteThemes, mNoteDates);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
     }
 
 }
